ba8e7279d2b797c52df1997d4d2eb11609bb7426
Clayton LZ <clayton@hauz.io>
2016-05-26 11:05:06 -0700

    initial

---
+++ Example/Pods/Target Support Files/MyExamplePod/MyExamplePod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MyExamplePod : NSObject
+@end
+@implementation PodsDummy_MyExamplePod
+@end
---
+++ Example/Pods/Target Support Files/MyExamplePod/MyExamplePod-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double MyExamplePodVersionNumber;
+FOUNDATION_EXPORT const unsigned char MyExamplePodVersionString[];
+
